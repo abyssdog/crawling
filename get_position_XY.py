8f01b7e2ab3d40436daac6a7cc4ec440a9064e41
abyssdog <abyssdog@dangamsoft.com>
2020-07-13 18:28:39 +0900

    카카오맵으로 추가 개발중

--- get_position_XY.py
+++ get_position_XY.py
@@ -9,7 +9,40 @@ class kakao_api:
     def __init__(self):
         self.key_api = "b3489265c0604b575bc22eda5da2e18f"
 
-    def get_location(self, addr1, addr2):
+    def get_location_single(self, addr, name):
+        # temp_addr = addr.split()
+        # search = temp_addr[0]+' '+temp_addr[1]+' '+temp_addr[2]+' '+name
+        search = addr + ' ' + name
+        try:
+            url = 'https://dapi.kakao.com/v2/local/search/address.json?query=' + search
+            headers = {"Authorization": "KakaoAK b3489265c0604b575bc22eda5da2e18f"}
+            result = json.loads(str(requests.get(url, headers=headers).text))
+            if len(result['documents']) != 0 and result['documents'][0]['address'] is not None:
+                match_first = result['documents'][0]['address']
+            elif len(result['documents']) != 0 and result['documents'][0]['address'] is None:
+                url = 'https://dapi.kakao.com/v2/local/search/address.json?query=' + addr + name
+                headers = {"Authorization": "KakaoAK b3489265c0604b575bc22eda5da2e18f"}
+                result = json.loads(str(requests.get(url, headers=headers).text))
+                if len(result['documents']) != 0:
+                    match_first = result['documents'][0]['address']
+                else:
+                    match_first = {
+                        'x': 0,
+                        'y': 0
+                    }
+            else:
+                match_first = {
+                    'x': 0,
+                    'y': 0
+                }
+        except Exception:
+            match_first = {
+                'x': 0,
+                'y': 0
+            }
+        return float(match_first['x']), float(match_first['y'])
+
+    def get_location_double(self, addr1, addr2):
         b = addr2.find(',')
         if b != -1:
             a2 = addr2[0:b]
@@ -65,16 +98,19 @@ class kakao_api:
             user='root'
         )
         curs = conn.cursor()
-        sql = """SELECT id, company_name, location_address, road_name_address
-                       FROM animal_sales"""
+        sql = """SELECT id, company, address
+                   FROM animal_company
+                  WHERE location_x = '0.0'"""
         curs.execute(sql)
         sql_rows = curs.fetchall()
         for row in sql_rows:
-            xy = ka.get_location(row[2], row[3])
+            # xy = ka.get_location(row[2], row[3])
+            xy = ka.get_location_single(row[2], row[1])
             _dict = {"id": row[0], "location_x": xy[0], "location_y": xy[1]}
             rows.append(_dict)
+            print('{} {} {} {} {}'.format(row[0],row[1],row[2],xy[0],xy[1]))
         for row in rows:
-            sql = """update animal_funeral
+            sql = """update animal_company
                         set location_x = {x}, location_y = {y}
                       where id = {id}""".format(x=row['location_x'], y=row['location_y'], id=row['id'])
             curs.execute(sql)
@@ -84,6 +120,6 @@ class kakao_api:
 
 if __name__ == '__main__':
     k = kakao_api()
-    # k.get_localdata()
-    a = k.get_location('우리집막내둥이', '')
-    print(a)
+    k.get_localdata()
+    # a = k.get_location_single('제주특별자치도 서귀포시 강정동', '도그앤캣')
+    # print(a)
